d225b1fdaba08d58475854b505c45c6a416a4a05
Vladyslav Johannessen <vladyslavu@uia.no>
2025-08-24 22:51:43 +0200

    Add assignment 1

---
+++ assignment_1/main.py
@@ -0,0 +1,16 @@
+﻿import cv2 as cv
+
+
+def print_image_information(image):
+    img = cv.imread(image)
+    width, height, channels = img.shape
+    print(f"width: {width}")
+    print(f"height: {height}")
+    print(f"Channels: {channels}")
+    print(f"size {img.size}")
+    print(f"data type: {img.dtype}")
+
+
+if __name__ == '__main__':
+    print_image_information("lena.png")
+
---
+++ assignment_1/web_cammera.py
@@ -0,0 +1,31 @@
+﻿import cv2 as cv
+from pathlib import Path
+
+class WebCamInfo:
+    width : int
+    height : int
+    fps : int
+
+
+def get_webcam_info() -> WebCamInfo:
+    camera = cv.VideoCapture(0)
+    cam_info = WebCamInfo()
+
+    cam_info.width = int(camera.get(cv.CAP_PROP_FRAME_WIDTH))
+    cam_info.height = int(camera.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+    cam_info.fps = camera.get(cv.CAP_PROP_FPS)
+
+    return cam_info
+
+
+if __name__ == '__main__':
+    web_cam_info : WebCamInfo = get_webcam_info()
+
+    Path("solutions").mkdir(parents=True, exist_ok=True)
+
+    with open("solutions/camera_outputs.txt", "w") as outfile:
+        outfile.write(f"Width: {web_cam_info.width}\n")
+        outfile.write(f"height: {web_cam_info.height}\n")
+        outfile.write(f"fps: {web_cam_info.fps}\n")
+
