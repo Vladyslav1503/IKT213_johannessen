caf8f8344dff9c8b3d4944c72ec9f8b9c8203b4b
Vladyslav Johannessen <vladyslavu@uia.no>
2025-09-02 15:47:39 +0200

    Add assignment 2

---
+++ assignment_2/main.py
@@ -0,0 +1,108 @@
+import cv2
+import cv2 as cv
+import numpy as np
+
+
+def padding(image, border_width=100):
+    reflect = cv.copyMakeBorder(image, border_width, border_width, border_width, border_width, cv.BORDER_REFLECT)
+
+    return reflect
+
+
+def crop(image, x_0, x_1, y_0, y_1):
+    return image[y_0:y_1, x_0:x_1]
+
+
+def resize(image, width, height):
+    return cv.resize(image, (width, height), interpolation=cv.INTER_AREA)
+
+
+def copy(image, emptyPictureArray):
+    width, height, channels = image.shape
+    emptyPictureArray = np.zeros((width, height, channels), dtype=np.uint8)
+
+    i = 0
+    for pixel in image:
+        emptyPictureArray[i] = pixel
+        i += 1
+
+    return emptyPictureArray
+
+
+def grayscale(image):
+    return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+
+def hsv(image):
+    return cv.cvtColor(image, cv.COLOR_BGR2HSV)
+
+
+def hoe_shifted(image, emptyPictureArray, hue):
+    MAX_VALUE = 255
+
+    width, height, channels = image.shape
+    emptyPictureArray = np.zeros((width, height, channels), dtype=np.uint8)
+
+    for i in range(height):
+        for j in range(width):
+            for k in range(channels):
+                value = int(image[i, j, k]) + hue
+                if  value > MAX_VALUE or value < 0:
+                    value = value % 256
+                emptyPictureArray[i, j, k] = value
+
+    return emptyPictureArray
+
+
+def smoothing(image):
+    return cv.GaussianBlur(image, (15, 15), cv.BORDER_DEFAULT)
+
+
+def rotation(image, rotation_angle):
+    match rotation_angle:
+        case 90:
+            return cv.rotate(image, cv.ROTATE_90_CLOCKWISE)
+        case 180:
+            return cv.rotate(image, cv.ROTATE_180)
+        case 270:
+            return cv.rotate(image, cv.ROTATE_90_COUNTERCLOCKWISE)
+        case _:
+            raise ValueError("Only 90, 180, or 270 degrees are supported.")
+
+
+if __name__ == "__main__":
+    img = cv.imread('lena-2.png')
+
+    width = img.shape[0]
+    height = img.shape[1]
+
+    padding_image = padding(img, border_width=100)
+    cv.imwrite('solutions/padding.png', padding_image)
+
+    crop_image = crop(img, 80, width, 80, height)
+    crop_image_width, crop_image_height, _ = crop_image.shape
+    crop_image = crop(crop_image, 0, crop_image_width - 150, 0, crop_image_height - 150)
+    cv.imwrite('solutions/cropped.png', crop_image)
+
+    resized_image = resize(crop_image, 200, 200)
+    cv.imwrite('solutions/resized.png', resized_image)
+
+    copy_image = copy(img, [])
+    cv.imwrite('solutions/copy.png', copy_image)
+
+    grey_image = grayscale(img)
+    cv.imwrite('solutions/grayscale.png', grey_image)
+
+    hsv_image = hsv(img)
+    cv.imwrite('solutions/hsv.png', hsv_image)
+
+    hoe_shifted_image = hoe_shifted(img, [], 50)
+    cv.imwrite('solutions/hoe_shifted.png', hoe_shifted_image)
+
+    smoothing_image = smoothing(img)
+    cv.imwrite('solutions/smoothing.png', smoothing_image)
+
+    rotated_image_90 = rotation(img, 90)
+    rotated_image_180 = rotation(img, 180)
+    cv.imwrite('solutions/rotated_image_90.png', rotated_image_90)
+    cv.imwrite('solutions/rotated_image_180.png', rotated_image_180)
